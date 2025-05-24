dde09e9e0c8e89227c4932b642bb0e47da490852
wizard2498 <awizard2498@gmail.com>
2025-05-24 13:39:22 +0530

    A simple GUI added.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # First_repo
 <br>
 <h3>Hi...</h3>
+<br>
+This is my first repo.
---
+++ hi.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+def click():
+    print("You have clicked this button.")
+
+
+window = Tk()
+
+window.title("Hello I am Neelmani")
+window.geometry("500x200")
+window.config(background="black")
+
+Label = Label(window,
+              text="Hello everyone, click the button to see a message",
+              font=("Arial", 14,),
+              fg="#00ff00",
+              bg="black")
+Label.pack()
+
+button = Button(window,
+                text="Click here!",
+                command=click,
+                font=('Comic Sans', 20,'underline'),
+                fg="#00ff00", bg="black",
+                activeforeground="#00ff00",
+                activebackground="black",
+                state=ACTIVE)
+button.pack()
+
+
+window.mainloop()
